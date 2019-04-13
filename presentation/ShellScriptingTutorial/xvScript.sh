f9a456e107f9a6eef5f1da1c7af60a3888f3c209
Arya Khaligh <bartararya@gmail.com>
2019-04-13 09:30:35 +0430

    Add download and run xv6 script.

---
+++ presentation/ShellScriptingTutorial/xvScript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Author: Arya Khaligh
+
+XV6_FOLDER="xv6-script"
+
+rm -rf ~/$XV6_FOLDER
+
+cd ~
+mkdir $XV6_FOLDER && cd $XV6_FOLDER
+
+mkdir xv6-public
+cp ~/ali/xv6-public/* xv6-public
+
+cd ~/${XV6_FOLDER}/xv6-public
+
+mkdir ../tmp
+make > ../tmp/make-log.txt
+
+if [ $? -ne 0 ]
+then
+    echo "Error while making xv6."
+    exit 1
+fi
+
+
+
