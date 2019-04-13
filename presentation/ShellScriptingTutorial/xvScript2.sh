f221d9cb56243e00e44304f557f0b2241a571530
Arya Khaligh <bartararya@gmail.com>
2019-04-13 19:06:50 +0430

    Add xvScript2

--- presentation/ShellScriptingTutorial/xvScript2.sh
+++ presentation/ShellScriptingTutorial/xvScript2.sh
@@ -2,3 +2,36 @@
 
 # Author: Arya Khaligh (bartararya@gmail.com)
 # Date: 2019 
+
+Clone () {
+    mkdir xv6-public
+    cp ~/ali/xv6-public/* xv6-public
+}
+
+MakeError () {
+    if [ $1 -ne 0 ]
+    then
+        echo $2
+        exit 1
+    else
+        echo $3
+    fi
+}
+
+GetInput
+
+XV6_FOLDER="xv6-script"
+
+rm -rf ~/$XV6_FOLDER || echo "Error while removing previous $XV6_FOLDER directory."
+
+mkdir ~/$XV6_FOLDER && cd ~/$XV6_FOLDER
+
+Clone
+
+cd ~/${XV6_FOLDER}/xv6-public
+
+mkdir ../tmp
+MakeError $? "Error while making." "Success."
+ 
+make qemu-nox 
+MakeError $? "Error running qemu." "Success."
