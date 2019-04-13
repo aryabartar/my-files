b4aee99b6cb9c67aeb55b40e98182630369d52ff
Arya Khaligh <bartararya@gmail.com>
2019-04-13 09:41:06 +0430

    Add y/n utility

--- presentation/ShellScriptingTutorial/xvScript.sh
+++ presentation/ShellScriptingTutorial/xvScript.sh
@@ -2,25 +2,37 @@
 
 # Author: Arya Khaligh
 
-XV6_FOLDER="xv6-script"
-
-rm -rf ~/$XV6_FOLDER
-
-cd ~
-mkdir $XV6_FOLDER && cd $XV6_FOLDER
-
-mkdir xv6-public
-cp ~/ali/xv6-public/* xv6-public
-
-cd ~/${XV6_FOLDER}/xv6-public
-
-mkdir ../tmp
-make > ../tmp/make-log.txt
-
-if [ $? -ne 0 ]
+read -p "Run script? [y/N] : " USER_INPUT
+if [ $USER_INPUT = 'y' ]
 then
-    echo "Error while making xv6."
-    exit 1
+    echo "yes"
+elif [ $USER_INPUT = 'N' ]
+then
+    echo "no"
+else
+    echo "wrong_input"
 fi
 
-make qemu-nox
+
+# XV6_FOLDER="xv6-script"
+
+# rm -rf ~/$XV6_FOLDER
+
+# cd ~
+# mkdir $XV6_FOLDER && cd $XV6_FOLDER
+
+# mkdir xv6-public
+# cp ~/ali/xv6-public/* xv6-public
+
+# cd ~/${XV6_FOLDER}/xv6-public
+
+# mkdir ../tmp
+# make > ../tmp/make-log.txt
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Error while making xv6."
+#     exit 1
+# fi
+
+# make qemu-nox
